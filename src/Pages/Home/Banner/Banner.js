62b736307953da72bda88b07e92111dc7b3348ae
asif3062 <asifbsb2002@gmail.com>
2022-11-14 19:30:27 +0600

    banner image added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,12 +3,12 @@ import bannerImg from '../../../assets/images/chair.png'
 const Banner = () => {
     return (
         <div className="hero px-5 py-10">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={bannerImg} className="max-w-sm rounded-lg shadow-2xl" alt='' />
-                <div>
-                    <h1 className="text-4xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+            <div className="hero-content flex-col justify-between items-center lg:flex-row-reverse">
+                <img src={bannerImg} className="md:w-1/2 w-full rounded-lg shadow-2xl" alt='' />
+                <div className='md:w-1/2 w-full'>
+                    <h1 className="text-4xl font-bold">Your New Smile Starts Here</h1>
+                    <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+                    <button className="py-2 px-5 bg-cyan-400 text-white rounded-md">Get Started</button>
                 </div>
             </div>
         </div>
