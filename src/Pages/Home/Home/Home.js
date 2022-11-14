d3de7ab18e705062c766efca20739afcad3ab071
asif3062 <asifbsb2002@gmail.com>
2022-11-14 19:08:06 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div className='mx-5 home-banner'>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
